ae5a4d96aa37b4a24ad60681c514a9c09ef897f0
GaPhil <gaphil.io@gmail.com>
2019-11-30 23:49:44 +0100

    Add commments to and instructions

--- official-c/and-instructions.s
+++ official-c/and-instructions.s
@@ -49,14 +49,15 @@
 859:	41 21 c7             	and    %eax,%r15d           // fault here 3
 
     FAULTS:
-3de:	21 f2                	xor    %esi,$0x00000000     // fault here 1 ==> 33 35 00 00 00 00
-40a:	21 f0                	xor    %esi,$0x00000000     // fault here 2 ==> 33 35 00 00 00 00
-43b:	21 c7                	xor    %eax,$0x00000000     // fault here 3 ==> 33 05 00 00 00 00
+3de:	21 f2                	xor    %esi,$0x00000000     // fault here 1 ==> 33 35 00 00 00 00   DONE +4
+40a:	21 f0                	xor    %esi,$0x00000000     // fault here 2 ==> 33 35 00 00 00 00   DONE +4
+43b:	21 c7                	xor    %eax,$0x00000000     // fault here 3 ==> 33 05 00 00 00 00   DONE +4
 
-70a:	21 c3                	xor    %eax,$0x00000000     // fault here 1 ==> 33 05 00 00 00 00
-746:	21 df                	xor    %ebx,$0x00000000     // fault here 2 ==> 33 1d 00 00 00 00
-766:	41 21 c7             	xor    %eax,$0x00000000     // fault here 3 ==> 33 05 00 00 00 00
+70a:	21 c3                	xor    %eax,$0x00000000     // fault here 1 ==> 33 05 00 00 00 00   DONE +4
+746:	21 df                	xor    %ebx,$0x00000000     // fault here 2 ==> 33 1d 00 00 00 00   DONE +4
+766:	41 21 c7             	xor    %eax,$0x00000000     // fault here 3 ==> 33 05 00 00 00 00   DONE +3
 
-82e:	41 21 f9             	xor    %edi,$0x00000000     // fault here 1 ==> 33 3d 00 00 00 00
-83a:	21 c8                	xor    %ecx,$0x00000000     // fault here 2 ==> 33 0d 00 00 00 00
-859:	41 21 c7             	xor    %eax,$0x00000000     // fault here 3 ==> 33 05 00 00 00 00
+82e:	41 21 f9             	xor    %edi,$0x00000000     // fault here 1 ==> 33 3d 00 00 00 00   DONE +3
+83a:	21 c8                	xor    %ecx,$0x00000000     // fault here 2 ==> 33 0d 00 00 00 00   DONE +4
+859:	41 21 c7             	xor    %eax,$0x00000000     // fault here 3 ==> 33 05 00 00 00 00   DONE +3
+// hex editor increases by 33 bytes
